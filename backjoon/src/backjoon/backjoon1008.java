b0f94a05656419ffc8690341d330052eec48499f
YJunsu03 <73802392+YJunsoo@users.noreply.github.com>
2021-12-10 15:27:59 +0900

    update
    1008 : int -> double 수정
    10430 : 수정 필요

--- backjoon/src/backjoon/backjoon1008.java
+++ backjoon/src/backjoon/backjoon1008.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class backjoon1008 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
 		
 		System.out.println(a/b);
 		
